950aa56855b93a0c57e30a768b54e800ba54aa29
sarah nguyen <107088382+Sarahpixel@users.noreply.github.com>
2023-02-19 20:22:07 +1100

    Spawn wave

--- Assets/Universal/Scripts/SpawnManager.cs
+++ Assets/Universal/Scripts/SpawnManager.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    //Enemy Reference
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+
+    //Powerup Reference
+    public GameObject powerupPrefab;
     void Start()
     {
-        
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+
     }
 
     
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        //keeps tracks for enemies whenevr they get destroyed
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
